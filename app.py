eee8379104ee9f9550af83cbfef5de874a516033
P21l03n <PolinaKaleshchuk@gmail.com>
2021-10-22 18:21:31 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+conn = psycopg2.connect(database="postgres",
+                        user="Polina2",
+                        password="12345",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+    records = list(cursor.fetchall())
+
+    return render_template('account.html', full_name=records[0][1])
